9eceb5f2d8791a30a46dc01f2fa03d7f18729226
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-05-19 22:01:33 +0200

    Waybar config

--- home/programs/graphical/wm/hyprland/waybar/default.nix
+++ home/programs/graphical/wm/hyprland/waybar/default.nix
@@ -9,13 +9,31 @@
     source = ./scripts/weather;
     executable = true;
   };
-  home.file.".config/waybar/config.jsonc" = {
-    target = ".config/waybar/config.jsonc";
-    source = ./config.jsonc;
-  };
   programs.waybar = {
     enable = true;
     catppuccin.enable = true;
     catppuccin.mode = "createLink";
+    style = ./style.css;
+    settings = {
+      mainBar = {
+        layer = "top";
+        height = 50;
+        margin-top = 0;
+        margin-left = 0;
+        margin-bottom = 0;
+        spacing = 0;
+        modules-left = [
+          "custom/oslogo"
+        ];
+        modules-center = [
+          "hyprland/workspaces"
+        ];
+        "custom/oslogo" = {
+          format = "";
+          interval = "once";
+          tooltip = false;
+        };
+      };
+    };
   };
 }
