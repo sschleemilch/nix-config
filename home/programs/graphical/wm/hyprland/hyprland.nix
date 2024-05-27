8a5b51c5479687b79ba4b99a4d1f0c993842d2de
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-05-27 21:22:59 +0200

    Fixed hyprland border

--- home/programs/graphical/wm/hyprland/hyprland.nix
+++ home/programs/graphical/wm/hyprland/hyprland.nix
@@ -28,7 +28,7 @@
         gaps_in = 3;
         gaps_out = 5;
         border_size = 2;
-        "col.active_border" = "0xffblueAlpha 0xff$mauveAlpha 45deg";
+        "col.active_border" = "0xff$overlay1Alpha";
         "col.inactive_border" = "0xff$surface1Alpha";
         layout = "dwindle";
       };
