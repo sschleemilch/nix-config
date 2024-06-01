1f05d9b795ee40c1bfdd7cc481a3feef24904481
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-06-01 22:36:38 +0200

    Changed terminal font to FiraCode

--- home/programs/graphical/common/foot.nix
+++ home/programs/graphical/common/foot.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       main = {
-        font = "JetBrainsMono Nerd Font:size=12";
+        font = "FiraCode Nerd Font:size=12";
         pad = "5x5";
       };
     };
