14b6289b24ae3d71d79ec71ee079d16d59dce283
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-06-03 23:06:59 +0200

    Starship config ff

--- home/programs/terminal/starship.nix
+++ home/programs/terminal/starship.nix
@@ -3,9 +3,16 @@
     enable = true;
     catppuccin.enable = true;
     settings = {
+      format = "$os$username$directory$git_branch$git_state$git_status$status$line_break$character";
+      directory = {
+        format = "[](fg:blue)[ $path](bold bg:blue fg:surface0)[](fg:blue) ";
+      };
+      username = {
+        format = "[](fg:red)[ $user](bold bg:red fg:surface0)[](fg:red) ";
+      };
       status = {
         disabled = false;
-        symbol = " ";
+        format = "[](fg:red)[ $status](bold bg:red fg:surface0)[](fg:red) ";
       };
       direnv = {
         disabled = false;
@@ -13,40 +20,38 @@
         style = "bold peach";
         symbol = " ";
       };
-      aws.symbol = "  ";
-      c.symbol = " ";
-      conda.symbol = " ";
-      crystal.symbol = " ";
-      dart.symbol = " ";
-      directory.read_only = " 󰌾";
-      docker_context.symbol = " ";
-      git_branch.symbol = " ";
-      golang.symbol = " ";
-      hostname.ssh_symbol = " ";
-      java.symbol = " ";
-      julia.symbol = " ";
-      kotlin.symbol = " ";
-      lua.symbol = " ";
-      memory_usage.symbol = "󰍛 ";
-      nix_shell.symbol = " ";
-      nodejs.symbol = " ";
-      os.symbols = {
-        Android = " ";
-        Arch = " ";
-        Debian = " ";
-        Fedora = " ";
-        Linux = " ";
-        Macos = " ";
-        NixOS = " ";
-        Raspbian = " ";
-        Ubuntu = " ";
-        Windows = "󰍲 ";
+      os = {
+        format = "[](fg:overlay2)[$symbol](bg:overlay2 fg:surface0)[](fg:overlay2) ";
+        disabled = true;
+        symbols = {
+          Android = " ";
+          Arch = " ";
+          Debian = " ";
+          Fedora = " ";
+          Linux = " ";
+          Macos = " ";
+          NixOS = " ";
+          Raspbian = " ";
+          Ubuntu = " ";
+          Windows = "󰍲 ";
+        };
       };
-      package.symbol = "󰏗 ";
-      perl.symbol = " ";
-      php.symbol = " ";
-      python.symbol = " ";
-      rust.symbol = " ";
+      git_branch = {
+        format = "[](fg:peach)[ $branch](bold bg:peach fg:surface0)";
+      };
+      git_state = {
+        style = "bold peach";
+      };
+      git_status = {
+        format = "[$ahead_behind $all_status](bold bg:peach fg:surface0)[](fg:peach) ";
+        up_to_date = " 󰓦";
+        conflicted = "󱐋";
+        stashed = " ";
+        modified = " ";
+        staged = "󱊽";
+        renamed = " ";
+        deleted = " ";
+      };
     };
   };
 }
