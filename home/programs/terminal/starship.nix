b214ecb2ec895705a301579941ebbd6a8dd518ea
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-06-04 23:15:18 +0200

    Connected bubbles

--- home/programs/terminal/starship.nix
+++ home/programs/terminal/starship.nix
@@ -3,25 +3,35 @@
     enable = true;
     catppuccin.enable = true;
     settings = {
-      format = "$os$username$directory$git_branch$git_state$git_status$status$line_break$character";
+      format = "[-](surface2)$os$username$directory$git_branch$git_state$git_status$fill$status$jobs[](surface2)$line_break$character";
       directory = {
-        format = "[](fg:blue)[ $path](bold bg:blue fg:surface0)[](fg:blue) ";
+        format = "[](fg:blue)[ $path](bold bg:blue fg:surface0)[](fg:blue)[-](surface2)";
       };
       username = {
-        format = "[](fg:red)[ $user](bold bg:red fg:surface0)[](fg:red) ";
+        format = "[](fg:red)[ $user](bold bg:red fg:surface0)[](fg:red)[-](surface2)";
+      };
+      fill = {
+        disabled = false;
+        symbol = "-";
+        style = "surface2";
       };
       status = {
         disabled = false;
-        format = "[](fg:red)[ $status](bold bg:red fg:surface0)[](fg:red) ";
+        format = "[](fg:red)[ $status](bold bg:red fg:surface0)[](fg:red)[-](surface2)";
       };
       direnv = {
         disabled = false;
-        format = "[$symbol$loaded/$allowed]($style) ";
+        format = "[$symbol$loaded/$allowed]($style)[-](surface2)";
         style = "bold peach";
         symbol = " ";
       };
+      jobs = {
+        format = "[](fg:subtext1)[$symbol$number](bold bg:subtext1 fg:surface0)[](fg:subtext1)[-](surface2)";
+        symbol = " ";
+        number_threshold = 1;
+      };
       os = {
-        format = "[](fg:overlay2)[$symbol](bg:overlay2 fg:surface0)[](fg:overlay2) ";
+        format = "[](fg:overlay2)[$symbol](bg:overlay2 fg:surface0)[](fg:overlay2)[-](surface2)";
         disabled = true;
         symbols = {
           Android = " ";
@@ -43,7 +53,7 @@
         style = "bold peach";
       };
       git_status = {
-        format = "[$ahead_behind $all_status](bold bg:peach fg:surface0)[](fg:peach) ";
+        format = "[$ahead_behind $all_status](bold bg:peach fg:surface0)[](fg:peach)[-](surface2)";
         up_to_date = " 󰓦";
         conflicted = "󱐋";
         stashed = " ";
