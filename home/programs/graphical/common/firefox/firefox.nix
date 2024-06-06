ddd1b55861009fd95a423e2aee70eabddb8ef207
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-06-06 21:33:49 +0200

    Firefox search engines, settings, kagi

--- home/programs/graphical/common/firefox/firefox.nix
+++ home/programs/graphical/common/firefox/firefox.nix
@@ -34,11 +34,98 @@
     };
     profiles.${config.home.username} = {
       id = 0;
+      settings = {
+        "browser.translations.neverTranslateLanguages" = "de";
+        # Disable browser crash reporting
+        "browser.tabs.crashReporting.sendReport" = false;
+        # disable bell sounds
+        "accessibility.typeaheadfind.enablesound" = false;
+        "device.sensors.enabled" = false; # This isn't a phone
+        "geo.enabled" = false; # Disable geolocation alltogether
+        "dom.push.enabled" = false; # no notifications, really...
+        "dom.push.connection.enabled" = false;
+        "dom.battery.enabled" = false; # you don't need to see my battery...
+      };
       name = "${config.home.username}";
       search = {
-        default = "Google";
-        privateDefault = "DuckDuckGo";
+        default = "Kagi";
         force = true;
+        order = [ "Kagi" "DuckDuckGo" "NixOS Options" "Nix Packages" "Home Manager" "GitHub" ];
+        engines = {
+          "Kagi" = {
+            iconUpdateURL = "https://kagi.com/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000;
+            definedAliases = [ "@k" ];
+            urls = [
+              {
+                template = "https://kagi.com/search";
+                params = [
+                  {
+                    name = "q";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+          };
+          "Nix Packages" = {
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@np" ];
+            urls = [
+              {
+                template = "https://search.nixos.org/packages";
+                params = [
+                  {
+                    name = "type";
+                    value = "packages";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+          };
+
+          "NixOS Options" = {
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@no" ];
+            urls = [
+              {
+                template = "https://search.nixos.org/options";
+                params = [
+                  {
+                    name = "channel";
+                    value = "unstable";
+                  }
+                  {
+                    name = "query";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+          };
+
+          "GitHub" = {
+            iconUpdateURL = "https://github.com/favicon.ico";
+            updateInterval = 24 * 60 * 60 * 1000;
+            definedAliases = [ "@gh" ];
+
+            urls = [
+              {
+                template = "https://github.com/search";
+                params = [
+                  {
+                    name = "q";
+                    value = "{searchTerms}";
+                  }
+                ];
+              }
+            ];
+          };
+        };
       };
       extensions = with inputs.firefox-addons.packages.${pkgs.system}; [
         ublock-origin
