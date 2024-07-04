594c2b0ba2dd683aeff0c3b3a8b103246511d292
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-07-04 22:46:41 +0200

    nvim: bufferline

--- home/programs/terminal/nvim/configs/lua/plugins/ui.lua
+++ home/programs/terminal/nvim/configs/lua/plugins/ui.lua
@@ -53,6 +53,33 @@ return {
 			},
 		},
 	},
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+			{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+			{ "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+			{ "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+			{ "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+			{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+			{ "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+			{ "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+			{ "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+		},
+		opts = {
+			options = {
+				always_show_bufferline = false,
+				separator_style = {"", ""},
+                indicator = {
+                    style = "none"
+                },
+                show_buffer_close_icons = false,
+			},
+		},
+	},
 	-- Better `vim.notify()`
 	{
 		"rcarriga/nvim-notify",
