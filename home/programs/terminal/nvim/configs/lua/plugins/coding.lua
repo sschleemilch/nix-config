22f99007211c5eee6beaa6b9afde29b38599cd06
Sebastian Schleemilch <sebastian.schleemilch@posteo.net>
2024-07-12 11:38:42 +0200

    nvim: todo-comments

--- home/programs/terminal/nvim/configs/lua/plugins/coding.lua
+++ home/programs/terminal/nvim/configs/lua/plugins/coding.lua
@@ -229,6 +229,36 @@ return {
 		opts = {},
 	},
 
+	{
+		"folke/todo-comments.nvim",
+		cmd = { "TodoTrouble", "TodoTelescope" },
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"]t",
+				function()
+					require("todo-comments").jump_next()
+				end,
+				desc = "Next Todo Comment",
+			},
+			{
+				"[t",
+				function()
+					require("todo-comments").jump_prev()
+				end,
+				desc = "Previous Todo Comment",
+			},
+			{ "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+			{
+				"<leader>xT",
+				"<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>",
+				desc = "Todo/Fix/Fixme (Trouble)",
+			},
+			{ "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
+			{ "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+		},
+	},
 	-- Better text-objects
 	{
 		"echasnovski/mini.ai",
